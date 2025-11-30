2826bd5efb59ffe57b3c7b249bf4a5c0df28cdc4
Dan Mahara <dan.mahara.0909@gmail.com>
2025-11-30 11:52:19 +0545

    optimize imports

--- src/main/java/com/ecommerce/controllers/admin/DashboardController.java
+++ src/main/java/com/ecommerce/controllers/admin/DashboardController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommerce.enums.PageTemplate;
-// import com.ecommerce.repository.UserRepository;
-import com.ecommerce.repository.FrontendRepository;
 import com.ecommerce.repository.admin.DashboardRepository;
 
 @Controller
