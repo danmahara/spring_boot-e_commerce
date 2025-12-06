5187c257fa70518df3dee1b5ff80e4c6bc241cf9
Dan Mahara <dan.mahara.0909@gmail.com>
2025-12-06 21:10:33 +0545

    added product delete method

--- src/main/java/com/ecommerce/controllers/admin/ProductController.java
+++ src/main/java/com/ecommerce/controllers/admin/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -265,6 +266,18 @@ public class ProductController {
             System.out.println("Error is: " + e.getMessage());
             return ResponseEntity.internalServerError().body(response);
         }
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Map<String, Object>> delete(@PathVariable Long id) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        productService.delete(id);
+        response.put("success", true);
+        response.put("message", "Product Deleted successfully");
+
+        return ResponseEntity.ok(response);
     }
 
 }
