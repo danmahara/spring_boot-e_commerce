558b4a0fc4cef7f4b27791a152ac38489693ec06
Dan Mahara <dan.mahara.0909@gmail.com>
2025-12-08 23:38:27 +0545

    added role controller to map files

---
+++ src/main/java/com/ecommerce/controllers/admin/RoleController.java
@@ -0,0 +1,16 @@
+package com.ecommerce.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin/roles")
+public class RoleController {
+
+    @GetMapping({"","/"})
+    public String index() {
+
+        return "admin/role/index";
+    }
+}
