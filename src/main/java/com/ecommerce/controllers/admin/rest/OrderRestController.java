392a37d1a26ab3300326eba850fc01d9bff50bf9
Dan Mahara <dan.mahara.0909@gmail.com>
2025-12-25 22:41:00 +0545

    feat/update: added order method to store user order

--- src/main/java/com/ecommerce/controllers/admin/rest/OrderRestController.java
+++ src/main/java/com/ecommerce/controllers/admin/rest/OrderRestController.java
@@ -5,14 +5,19 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.dtos.response.ApiResponse;
 import com.ecommerce.models.Order;
+import com.ecommerce.requests.MakeOrderRequest;
 import com.ecommerce.security.CustomUserDetails;
 import com.ecommerce.services.OrderService;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -27,6 +32,20 @@ public class OrderRestController {
         List<Order> orders = orderService.findAllOrdersByUser(userDetails.getId());
 
         return ResponseEntity.ok(ApiResponse.success("Orders retrived successfully", orders));
+    }
+
+    @PostMapping("/make-order")
+    public ResponseEntity<ApiResponse<Order>> order(
+            @AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam Long id,
+            @Valid @RequestBody MakeOrderRequest request) {
+
+        try {
+            Order order = orderService.createOrder(id, request);
+            return ResponseEntity.ok(ApiResponse.success("Order Placed Successfully", order));
+        } catch (Exception e) {
+            return ResponseEntity.ok(ApiResponse.error("Order Placed Failed", e.getMessage()));
+
+        }
     }
 
 }
