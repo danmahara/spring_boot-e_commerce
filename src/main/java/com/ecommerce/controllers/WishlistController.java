42d2a361747e41d9bebac784ffbde69e39d8d8b7
Dan Mahara <dan.mahara.0909@gmail.com>
2026-01-02 23:37:08 +0545

    added method to return wishlist items

--- src/main/java/com/ecommerce/controllers/WishlistController.java
+++ src/main/java/com/ecommerce/controllers/WishlistController.java
@@ -1,13 +1,20 @@
 package com.ecommerce.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommerce.models.User;
+import com.ecommerce.models.Wishlist;
+import com.ecommerce.models.admin.Image;
+import com.ecommerce.models.admin.Product;
 import com.ecommerce.repository.WishListRepository;
+import com.ecommerce.repository.admin.ImageRepository;
 import com.ecommerce.security.CustomUserDetails;
 import com.ecommerce.services.UserService;
 
@@ -20,6 +27,33 @@ public class WishlistController {
 
     private final WishListRepository wishListRepository;
     private final UserService userService;
+    private final ImageRepository imageRepository;
+
+    @GetMapping
+    public String index(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+
+        User user = userService.findUserById(userDetails.getId());
+
+        List<Wishlist> wishlists = wishListRepository.findAllByUser(user);
+
+        model.addAttribute("wishlists", wishlists);
+
+        List<Product> products = new ArrayList<>();
+
+        for (Wishlist item : wishlists) {
+            products.add(item.getProduct());
+
+        }
+
+        for (Product p : products) {
+            List<Image> imgs = imageRepository.findByImageableTypeAndImageableId("product", p.getId());
+            p.setImages(imgs);
+        }
+
+        model.addAttribute("user", user);
+
+        return "pages/customer/wishlist";
+    }
 
     @GetMapping("/count")
     public long countWishlistItem(@AuthenticationPrincipal CustomUserDetails userDetails) {
--- src/main/java/com/ecommerce/repository/WishListRepository.java
+++ src/main/java/com/ecommerce/repository/WishListRepository.java
@@ -1,8 +1,11 @@
 package com.ecommerce.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ecommerce.models.User;
 import com.ecommerce.models.Wishlist;
@@ -16,5 +19,7 @@ public interface WishListRepository extends JpaRepository<Wishlist, Long> {
     boolean existsByUserIdAndProductId(Long userId, Long productId);
 
     Optional<Wishlist> findByUserIdAndProductId(Long userId, Long productId);
+
+    List<Wishlist> findAllByUser(User user);
 
 }
